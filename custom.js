f70f0bc2de2863717649ee215ab38be366ac012a
gillianmaloy@gmail.com <gillianmaloy@gmail.com>
2017-12-17 14:57:08 -0500

    Added welcome/product image rotation

---
+++ custom.js
@@ -0,0 +1,13 @@
+var slidepics = ["https://github.com/gillianglynn/gillianglynn.github.io/blob/master/Assets/Carousel/wesite%20welcome.jpg?raw=true",
+"https://github.com/gillianglynn/gillianglynn.github.io/blob/master/Assets/Carousel/backpack-front.jpg?raw=true",
+"https://github.com/gillianglynn/gillianglynn.github.io/blob/master/Assets/Carousel/pin-ata-front.jpg?raw=true"]
+$(document).ready(function(){
+  console.log("test")
+i=0
+$("#picslides img").attr("src", slidepics[i])
+myTimer=setInterval(function(){
+    i=(i+1)%3
+    $("#picslides img").attr("src", slidepics[i])
+  }
+, 15000)
+})
